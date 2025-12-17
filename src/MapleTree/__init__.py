7b1464e573b6278a20c79b318dbd44c83e2931b5
Ryuji Hazama <ryuji_hazama@hotmail.com>
2025-12-18 01:32:40 +0900

    Update to v2.0(2)
    Add log level functions
    Add __init__.py file

---
+++ src/MapleTree/__init__.py
@@ -0,0 +1,11 @@
+"""
+Docstring for MapleTree
+"""
+
+from .MapleTree import MapleTree, Logger
+
+__all__ = ['MapleTree', 'Logger']
+
+__version__ = "2.0.0"
+__author__ = "Ryuji Hazama"
+__license__ = "MIT"
